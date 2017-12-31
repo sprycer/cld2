a91563480696347f07a59dd3a8697f081df5ad58
lemig <lemig@users.noreply.github.com>
2017-12-31 13:10:14 +0100

    Fix Mac OS compile issue
    https://github.com/CLD2Owners/cld2/issues/41

--- internal/compile_libs.sh
+++ internal/compile_libs.sh
@@ -37,7 +37,7 @@ g++ $CFLAGS $CPPFLAGS $CXXFLAGS -shared -fPIC \
   cld_generated_cjk_delta_bi_4.cc generated_distinct_bi_0.cc  \
   cld2_generated_quadchrome_2.cc cld2_generated_deltaoctachrome.cc \
   cld2_generated_distinctoctachrome.cc  cld_generated_score_quad_octa_2.cc  \
-  -o libcld2.so $LDFLAGS -Wl,-soname=libcld2.so
+  -o libcld2.dylib $LDFLAGS -Wl,-install_name,libcld2.dylib
 
 g++ $CFLAGS $CPPFLAGS $CXXFLAGS -shared -fPIC \
   cldutil.cc cldutil_shared.cc compact_lang_det.cc compact_lang_det_hint_code.cc \
@@ -49,5 +49,4 @@ g++ $CFLAGS $CPPFLAGS $CXXFLAGS -shared -fPIC \
   cld_generated_cjk_delta_bi_32.cc generated_distinct_bi_0.cc  \
   cld2_generated_quad0122.cc cld2_generated_deltaocta0122.cc \
   cld2_generated_distinctocta0122.cc  cld_generated_score_quad_octa_0122.cc  \
-  -o libcld2_full.so $LDFLAGS -Wl,-soname=libcld2_full.so
-
+  -o libcld2_full.dylib $LDFLAGS -Wl,-install_name,libcld2_full.dylib
